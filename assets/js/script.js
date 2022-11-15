c62e21c23afdea8cd032019fb6be50361ccbb27a
Fidgino <Fidgino@gmail.com>
2022-11-15 17:20:23 +0200

    interface css terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 /* Mode strict */
+
 "use strict";
+
 /* array with punchlines
 1- Nekfeu
 2- Hugo TSR
 3- Freeze Corleone
-4- Yuzmv
+4- Vald
 */
+
 let listPunchline = [
     ['Soit tu subis, soit tu mets les coups', 'Le passé fut turbulent et nous réserve un futur brûlant', 'Les jeunes pensent plus à des stars débiles qu’à Martin Luther King', 'Indépendants comme PNL, on veut être les premiers comme Jul'],
     ['Si la galère était un sport j’aurais ma place aux JO de Londres', 'Tu taffes pour des bourges que tu rembourses à chaque achat que tu fais', 'La réussite s’rait la bienvenue comme un quatre quart en Somalie', 'Paraît qu’on a d’la chance d’avoir le smic et d’s’endormir avec des draps C’est comme dire à un mec sans mains qu’il a d’la chance d’avoir des bras'],
@@ -13,11 +16,18 @@ let listPunchline = [
     ['Il a pas dit bonjour : du coup, il s’est fait niquer sa mère', 'Pour 80 balles j’pouvais même pas lui glisser', 'Je pense pas à toi si je pense au futur', 'On efface tout quand je crache, j’ai trempé ma bite dans un pot d’Tipp-Ex', 'Si tu l’fais pas, moi, je l’fais ; si tu l’sais pas, moi, je l’sais comme si j’étais sorti d’H.E.C'],
 ];
 
+/* Random Maths */
+
+const random = Math.floor(Math.random() * listPunchline.length);
+const random2 = Math.floor(Math.random() * listPunchline[random].length);
+console.log(random, listPunchline[random][random2]);
+
 /* DOM ELEMENTS */
 
 let startButton = document.querySelector('.startBtn');
 let napsImg = document.querySelector('.napsImg');
-let okaySong = document.querySelector('.Okay')
+let okaySong = document.querySelector('.Okay');
+let pGame = document.querySelector('.containerGame');
 
 /* Button start game */
 
@@ -29,6 +39,7 @@ startButton.addEventListener('click', function() {
         okaySong.play();
         setTimeout(() => {
             napsImg.remove()
+            pGame.classList.add('block')
           }, 5700)
       }, 1500)
 })
